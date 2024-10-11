a5f6ac3202233cbd87d207cb11cd009487562a23
TheSingularity-42 <rrkob@yahoo.com.au>
2024-10-11 20:01:57 +1100

    ahhhh

---
+++ Code/vises.js
@@ -0,0 +1,6 @@
+var spec1 = "Code/final_code/pie.vg.json";
+vegaEmbed('#pie', spec1).then(function(result) {}).catch(console.error);
+
+
+var spec2 = "Code/final_code/large_vis.vg.json";
+vegaEmbed('#large_vis', spec2).then(function(result) {}).catch(console.error);
