e188b19d0b81fe2f35fc73f233193c64a1a272df
TheSingularity-42 <rrkob@yahoo.com.au>
2024-10-15 00:10:23 +1100

    lots of stuff

--- Code/vises.js
+++ Code/vises.js
@@ -4,3 +4,9 @@ vegaEmbed('#pie', spec1).then(function(result) {}).catch(console.error);
 
 var spec2 = "Code/final_code/large_vis.vg.json";
 vegaEmbed('#large_vis', spec2).then(function(result) {}).catch(console.error);
+
+var spec23 = "Code/final_code/genderbar.vg.json";
+vegaEmbed('#gender', spec3).then(function(result) {}).catch(console.error);
+
+var spec4 = "Code/final_code/barpie.vg.json";
+vegaEmbed('#barpie', spec4).then(function(result) {}).catch(console.error);
